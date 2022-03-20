80a35cf5ef315ad0baa5c8a2ea31225db5250eb0
minormending <54426030+minormending@users.noreply.github.com>
2022-03-20 18:09:02 -0400

    add argparse support

--- py_jetblue/jetblue.py
+++ py_jetblue/jetblue.py
@@ -211,28 +211,8 @@ class JetBlueParser:
         return result
 
 
-async def main() -> None:
-    """
-    passengers = PassengerInfo(adults=1)
-    async with JetBluePuppet(debug=True) as client:
-        resp = await client.get_fares_json("JFK", "MIA", datetime(2022, 6, 2), datetime(2022, 6, 6), passengers)
-    """
-    with open("example.json", "r") as f:
-        contents = f.read()
-        import json
-
-        jc = json.loads(contents)
-        resp = JetBluePuppetResponse(**jc)
-    j = JetBlueParser.parse(resp)
-    from pprint import pprint
-
-    pprint(j)
-
-
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
-
-    """import argparse
+    import argparse
 
     parser = argparse.ArgumentParser(description="Get JetBlue airline prices.")
     parser.add_argument("origin", help="Origin airport.")
@@ -256,25 +236,20 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    passengers = PassengerInfo(Adults=args.passengers, Children=args.children)
+    async def main() -> None:
+        passengers = PassengerInfo(adults=args.passengers, children=args.children)
+        """async with JetBluePuppet(debug=True) as client:
+            resp = await client.get_fares_json(args.origin, args.destination, args.departure_date, args.return_date, passengers)
+        """
+        with open("example.json", "r") as f:
+            contents = f.read()
+            import json
 
-    client = JetBlue()
-    departure_info = client.get_departure_info_for(
-        args.origin, args.destination, passengers, args.departure_date
-    )
-    if departure_info:
-        print(departure_info)
-        return_info = client.get_return_info_for(
-            args.origin,
-            args.destination,
-            passengers,
-            args.departure_date,
-            args.return_date,
-        )
-        if return_info:
-            print(return_info)
-            print(
-                f"Total price: ${departure_info.price + return_info.price} " +
-                f"for {departure_info.day} to {return_info.day} " + 
-                f"from {departure_info.departure_airport} to {return_info.departure_airport}"
-            )"""
+            jc = json.loads(contents)
+            resp = JetBluePuppetResponse(**jc)
+        j = JetBlueParser.parse(resp)
+        from pprint import pprint
+
+        pprint(j)
+
+    asyncio.get_event_loop().run_until_complete(main())
