76cefbe6c3078597d59200ee87d53186f5a88d47
minormending <54426030+minormending@users.noreply.github.com>
2022-03-20 10:43:35 -0400

    use pyppeteer to get jetblue data

--- py_jetblue/jetblue.py
+++ py_jetblue/jetblue.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from datetime import datetime
-import requests
 from typing import List, Union, Dict
 import asyncio
 from pyppeteer import launch
@@ -8,23 +7,49 @@ from pyppeteer import launch
 
 @dataclass
 class PassengerInfo:
-    Adults: int
-    Children: int = 0
-    Infants: int = 0
+    adults: int
+    children: int = 0
+    infants: int = 0
+
+@dataclass
+class FareInfo:
+    itineraryID: str
+    price: float
+    cabinclass: str
+    refundable: bool
+    status: str
 
 
-class JetBlue:
-        def __init__(self) -> None:
-            self.session = requests.Session()
+class JetBluePuppet:
+    def __init__(self) -> None:
+        self.browser = None
+
+    async def get_fares_json(self) -> dict:
+        if not self.browser:
+            self.browser = await launch()
+        page = await self.browser.newPage()
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36')
+        try:
+            await page.goto('https://www.jetblue.com/booking/flights?from=NYC&to=MIA&depart=2022-06-02&return=2022-06-06&isMultiCity=false&noOfRoute=1&lang=en&adults=1&children=0&infants=0&sharedMarket=false&roundTripFaresFlag=false&usePoints=false')
+            resp = await page.waitForResponse(lambda r: 'outboundLFS' in r.url, timeout=30000)
+            content = await resp.text()
+            with open('example1.json', 'w') as f:
+                f.write(content)
+        finally:
+            await page.screenshot({'path': 'example1.png'})
+            await self.browser.close()
+
+    async def __aenter__(self):
+        return self
+    
+    async def __aexit__(self, exc_type, exc, tb):
+        if self.browser:
+            await self.browser.close()
+
 
 async def main():
-    browser = await launch({
-    'ignoreDefaultArgs': ['--disable-extensions'],
-    })
-    page = await browser.newPage()
-    await page.goto('https://www.jetblue.com/booking/flights?from=NYC&to=MIA&depart=2022-06-02&return=2022-06-06&isMultiCity=false&noOfRoute=1&lang=en&adults=1&children=0&infants=0&sharedMarket=false&roundTripFaresFlag=false&usePoints=false')
-    await page.screenshot({'path': 'example.png'})
-    await browser.close()
+    async with JetBluePuppet() as client:
+        await client.get_fares_json()
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
